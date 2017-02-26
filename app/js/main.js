10652ae4b93ad4eaf4ed002e84894c09225208ed
jisegura <segurajuanignacio@outlook.com>
2017-02-26 13:14:30 -0300

    lot of things

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,13 @@
 'use strict';
 
+$('.box-foto a').hover(
+  function() {
+    $('.tlt').textillate('in');
+  }, function() {
+    // out
+  }
+);
+
 // $(document).ready(function () {
 //  $('#afiche').click(function () {
 //    // body...
