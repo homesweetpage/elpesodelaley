8e64623a18f558116709ba756c3366930c891f18
tgispert <spfury@gmail.com>
2017-02-26 18:30:22 -0300

    hover en personajes

--- app/js/typewriter.js
+++ app/js/typewriter.js
@@ -1,34 +1,71 @@
 $(document).ready(function () {
-    var typewriterEnAnimacion = false;
-    
-    function typeWriter(elem,text, i) {
+
+    var enAnimBarrientos = { val: false };
+    var enAnimGrandinetti = { val: false };
+    var enAnimOnetto = { val: false };
+    var enAnimMiras = { val: false };
+    var enAnimAruzzi = { val: false };
+    var enAnimBarassi = { val: false };
+
+    function typeWriter(elem,text,i,enAnim) {
+        
         if (i < (text.length)) {
-            elem.html(text.substring(0, i+1));
-            setTimeout(function() {
-                typeWriter(elem,text, i + 1)
-            }, 70);
+                elem.html(text.substring(0, i+1));
+                setTimeout(function() {
+                    typeWriter(elem,text, i + 1,enAnim)
+                }, 20);
         }
         else{
-            typewriterEnAnimacion = false;
+            enAnim.val = false;
         }
     };
 
-    $('#pj-aruzzi').hover(function(){
-        var elem = $(this).parent().next().find('.typewriter');
-        if(typewriterEnAnimacion == false){
-            if(elem.html() !== "Betty"){
-                typewriterEnAnimacion = true;
-                typeWriter(elem,"Betty",0);
+    function typeWrite(pj,estado,enAnim){
+        var elem = pj.parent().next().find('.typewriter');
+        var str = pj.children().attr("alt").split('-');
+        if(enAnim.val == false){
+            if(elem.html() !== str[estado]){
+                enAnim.val = true;
+                console.log(enAnimBarrientos.val,enAnimGrandinetti.val,enAnimMiras.val,enAnimAruzzi.val,enAnimBarassi.val);
+                typeWriter(elem,str[estado],0,enAnim);
             }      
         }
+    }
+
+    $('#pj-barrientos').hover(function(){
+        typeWrite($(this),1,enAnimBarrientos);
     },function(){
-        var elem = $(this).parent().next().find('.typewriter');
-        if(typewriterEnAnimacion == false){
-            if(elem.html() !== "Jorgelina Aruzzi"){
-                typewriterEnAnimacion = true;
-                typeWriter(elem,"Jorgelina Aruzzi",0);
-            }
-        }
+         typeWrite($(this),0,enAnimBarrientos);
+    });
+
+    $('#pj-grandinetti').hover(function(){
+        typeWrite($(this),1,enAnimGrandinetti);
+    },function(){
+         typeWrite($(this),0,enAnimGrandinetti);
+    });
+
+    $('#pj-onetto').hover(function(){
+        typeWrite($(this),1,enAnimOnetto);
+    },function(){
+         typeWrite($(this),0,enAnimOnetto);
+    });
+
+    $('#pj-miras').hover(function(){
+        typeWrite($(this),1,enAnimMiras);
+    },function(){
+         typeWrite($(this),0,enAnimMiras);
+    });
+
+    $('#pj-aruzzi').hover(function(){
+        typeWrite($(this),1,enAnimAruzzi);
+    },function(){
+         typeWrite($(this),0,enAnimAruzzi);
+    });
+
+    $('#pj-barassi').hover(function(){
+        typeWrite($(this),1,enAnimBarassi);
+    },function(){
+         typeWrite($(this),0,enAnimBarassi);
     });
 
 });
