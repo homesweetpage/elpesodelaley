c8f8fc43cb2d5e7039c2e1c9212e7a497221fa3c
tgispert <spfury@gmail.com>
2017-02-27 17:32:20 -0300

    fix final en typewriter.js

--- app/js/typewriter.js
+++ app/js/typewriter.js
@@ -34,37 +34,61 @@ $(document).ready(function () {
     $('#pj-barrientos').hover(function(){
         typeWrite($(this),1,enAnimBarrientos);
     },function(){
-         typeWrite($(this),0,enAnimBarrientos);
+        typeWrite($(this),0,enAnimBarrientos);
+        if(!$('#pj-barrientos').is(":hover")){
+            setTimeout(function(){
+            typeWrite($('#pj-barrientos'),0,enAnimBarrientos);
+        },800);}
     });
 
     $('#pj-grandinetti').hover(function(){
         typeWrite($(this),1,enAnimGrandinetti);
     },function(){
-         typeWrite($(this),0,enAnimGrandinetti);
+        typeWrite($(this),0,enAnimGrandinetti);
+        if(!$('#pj-grandinetti').is(":hover")){
+            setTimeout(function(){
+            typeWrite($('#pj-grandinetti'),0,enAnimGrandinetti);
+        },800);}
     });
 
     $('#pj-onetto').hover(function(){
         typeWrite($(this),1,enAnimOnetto);
     },function(){
-         typeWrite($(this),0,enAnimOnetto);
+        typeWrite($(this),0,enAnimOnetto);
+        if(!$('#pj-onetto').is(":hover")){
+            setTimeout(function(){
+            typeWrite($('#pj-onetto'),0,enAnimOnetto);
+        },800);}
     });
 
     $('#pj-miras').hover(function(){
         typeWrite($(this),1,enAnimMiras);
     },function(){
-         typeWrite($(this),0,enAnimMiras);
+        typeWrite($(this),0,enAnimMiras);
+        if(!$('#pj-miras').is(":hover")){
+            setTimeout(function(){
+            typeWrite($('#pj-miras'),0,enAnimMiras);
+        },800);}
     });
 
     $('#pj-aruzzi').hover(function(){
         typeWrite($(this),1,enAnimAruzzi);
     },function(){
-         typeWrite($(this),0,enAnimAruzzi);
+        typeWrite($(this),0,enAnimAruzzi);
+        if(!$('#pj-aruzzi').is(":hover")){
+            setTimeout(function(){
+            typeWrite($('#pj-aruzzi'),0,enAnimAruzzi);
+        },800);}
     });
 
     $('#pj-barassi').hover(function(){
         typeWrite($(this),1,enAnimBarassi);
     },function(){
          typeWrite($(this),0,enAnimBarassi);
+         if(!$('#pj-barassi').is(":hover")){
+            setTimeout(function(){
+            typeWrite($('#pj-barassi'),0,enAnimBarassi);
+        },800);}
     });
 
 });
