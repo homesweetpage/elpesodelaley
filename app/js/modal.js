1053669f619adebce05c56a81d09fd355660cd53
jisegura <segurajuanignacio@outlook.com>
2017-03-01 15:09:50 -0300

    modales

--- app/js/modal.js
+++ app/js/modal.js
@@ -11,11 +11,13 @@ var createModal = {
     return $image;
   },
   video: function() {
-    var $trailer = $('<iframe src="https://player.vimeo.com/video/63602119?title=0&byline=0&portrait=0" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>');
-    return $trailer;
+    var $video = $('<iframe src="https://player.vimeo.com/video/63602119?title=0&byline=0&portrait=0" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>');
+    var $div = $('<div class="video-container"></div>');
+    $video.appendTo($div);
+    return $div;
   },
   carousel: function() {
-    var $carousel = $('<section id="galeria2" class="carousel slide carousel-fade" data-ride="carousel"><div class="carousel-inner" role="listbox"><div class="item active"><img src="images/postales/lldll_a01_035987.jpg" data-color="lightblue" alt="First Image"></div><div class="item"><img src="images/postales/lldll_a03_027544.jpg" data-color="lightyellow" alt="Second Image"></div><div class="item"><img src="images/postales/lldll_a03_035764.jpg" data-color="lightred" alt="Third Image"></div><div class="item"><img src="images/postales/lldll_a03_035879.jpg" data-color="lightgreen" alt="Fourth Image"></div></div><a class="left carousel-control carousel-control-left" href="#galeria2" role="button" data-slide="prev"><img src="images/arrow-left-h.png" alt="Left Arrow" /><span class="sr-only">Previous</span></a><a class="right carousel-control carousel-control-right" href="#galeria2" role="button" data-slide="next"><img src="images/arrow-right-h.png" alt="Right Arrow" /><span class="sr-only">Next</span></a></section>');
+    var $carousel = $('<section id="mgaleria" class="carousel slide carousel-fade" data-ride="carousel"><div class="carousel-inner" role="listbox"><div class="item active"><img src="images/postales/lldll_a01_035987.jpg" data-color="lightblue" alt="First Image"></div><div class="item"><img src="images/postales/lldll_a03_027544.jpg" data-color="lightyellow" alt="Second Image"></div><div class="item"><img src="images/postales/lldll_a03_035764.jpg" data-color="lightred" alt="Third Image"></div><div class="item"><img src="images/postales/lldll_a03_035879.jpg" data-color="lightgreen" alt="Fourth Image"></div></div><a class="left carousel-control carousel-control-left" href="#mgaleria" role="button" data-slide="prev"><img src="images/arrow-left-h.png" alt="Left Arrow" /><span class="sr-only">Previous</span></a><a class="right carousel-control carousel-control-right" href="#mgaleria" role="button" data-slide="next"><img src="images/arrow-right-h.png" alt="Right Arrow" /><span class="sr-only">Next</span></a></section>');
     return $carousel;
   }
 };
@@ -38,14 +40,26 @@ function modalAfiche() {
 function modalTrailer() {
   // $('#avance').click(function () {
   //   var $video = createModal.video();
-  //   modalInsert($video);
+  //   var $container = createModal.container($video);
+  //   modalInsert($container);
   // });
 }
 
 function modalCarousel() {
   $('#avance').click(function () {
     var $carousel = createModal.carousel();
-    modalInsert($carousel);
+    var $container = createModal.container($carousel);
+    modalInsert($container);
+  });
+}
+
+function modalCast() {
+  $('#pj-barrientos').click(function(event) {
+    event.preventDefault();
+    var $img = createModal.img();
+    var $container = createModal.container($img);
+    $img.attr('src', 'images/postales/lldll_a03_027544.jpg');
+    modalInsert($container);
   });
 }
 
@@ -53,6 +67,7 @@ function modalReady() {
   modalAfiche();
   modalTrailer();
   modalCarousel();
+  modalCast();
 }
 
 $(document).ready(modalReady);
