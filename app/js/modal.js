2ded273c86758e044c187de1f6379695c05a410d
tgispert <spfury@gmail.com>
2017-03-05 03:44:07 -0300

    cambios generales + font courier prime + como podria quedar sinopsis?

--- app/js/modal.js
+++ app/js/modal.js
@@ -23,6 +23,10 @@ var createModal = {
   carouselPrensa: function() {
     var $carouselPrensa = $('<section id="mgaleria" class="carousel slide carousel-fade" data-ride="carousel"><div class="carousel-inner" role="listbox"><div class="item active"><img src="images/prensa/prensa-barrientos, grandinetti, onetto.jpg" data-color="lightblue" alt="Barrientos - Grandinetti - Onetto"></div><div class="item"><img src="images/prensa/prensa-barrientos.jpg" data-color="lightyellow" alt="Paola Barrientos"></div><div class="item"><img src="images/prensa/prensa-grandinetti.jpg" data-color="lightred" alt="Darío Grandinetti"></div><div class="item"><img src="images/prensa/prensa-onetto.jpg" data-color="lightgreen" alt="María Onetto"></div><div class="item"><img src="images/prensa/prensa-miras.jpg" data-color="green" alt="Fernán Mirás"></div><div class="item"><img src="images/prensa/prensa-miras1.jpg" data-color="green" alt="Fernán Mirás"></div><div class="item"><img src="images/prensa/prensa-todos.jpg" data-color="red" alt="Fernán Mirás, Paola Barrientos, Darío Grandinetti, María Onetto"></div><div class="item"><img src="images/prensa/prensa-todos1.jpg" data-color="yellow" alt="Fernán Mirás, Paola Barrientos, Darío Grandinetti, María Onetto"></div></div><a class="left carousel-control carousel-control-left" href="#mgaleria" role="button" data-slide="prev"><img src="images/arrow-left-h.png" alt="Left Arrow" /><span class="sr-only">Previous</span></a><a class="right carousel-control carousel-control-right" href="#mgaleria" role="button" data-slide="next"><img src="images/arrow-right-h.png" alt="Right Arrow" /><span class="sr-only">Next</span></a></section>');
     return $carouselPrensa;
+  },
+  sinopsis: function() {
+    var $sinopsis = $('<img >');
+    return $sinopsis;
   }
 };
 
@@ -67,56 +71,65 @@ function modalCast() {
     event.preventDefault();
     var $img = createModal.img();
     var $container = createModal.container($img);
-    $img.attr('src', 'images/postales/lldll_a02_032526.jpg');
+    $img.attr('src', 'images/postales/personajes/lldll_a02_032526.jpg');
     modalInsert($container);
   });
   $('#pj-onetto').click(function(event) {
     event.preventDefault();
     var $img = createModal.img();
     var $container = createModal.container($img);
-    $img.attr('src', 'images/postales/lldll_a03_035879.jpg');
+    $img.attr('src', 'images/postales/galeria/lldll_a03_035879.jpg');
     modalInsert($container);
   });
   $('#pj-grandinetti').click(function(event) {
     event.preventDefault();
     var $img = createModal.img();
     var $container = createModal.container($img);
-    $img.attr('src', 'images/postales/lldll_a03_035764.jpg');
+    $img.attr('src', 'images/postales/galeria/lldll_a03_035764.jpg');
     modalInsert($container);
   });
   $('#pj-miras').click(function(event) {
     event.preventDefault();
     var $img = createModal.img();
     var $container = createModal.container($img);
-    $img.attr('src', 'images/postales/lldll_a03_028969.jpg');
+    $img.attr('src', 'images/postales/personajes/lldll_a03_028969.jpg');
     modalInsert($container);
   });
   $('#pj-aruzzi').click(function(event) {
     event.preventDefault();
     var $img = createModal.img();
     var $container = createModal.container($img);
-    $img.attr('src', 'images/postales/aruzzi.jpg');
+    $img.attr('src', 'images/postales/personajes/aruzzi.jpg');
     modalInsert($container);
   });
   $('#pj-barassi').click(function(event) {
     event.preventDefault();
     var $img = createModal.img();
     var $container = createModal.container($img);
-    $img.attr('src', 'images/postales/lldll_a03_034472.jpg');
+    $img.attr('src', 'images/postales/personajes/barassi.jpg');
     modalInsert($container);
   });
   $('#pj-rosso').click(function(event) {
     event.preventDefault();
     var $img = createModal.img();
     var $container = createModal.container($img);
-    $img.attr('src', 'images/postales/nestor fabian.png');
+    $img.attr('src', 'images/postales/personajes/rosso.jpg');
     modalInsert($container);
   });
   $('#pj-lambertini').click(function(event) {
     event.preventDefault();
     var $img = createModal.img();
     var $container = createModal.container($img);
-    $img.attr('src', 'images/postales/lambertini.png');
+    $img.attr('src', 'images/postales/personajes/lambertini.jpg');
+    modalInsert($container);
+  });
+}
+
+function modalSinopsis() {
+  $('#sinopsis-completa').click(function () {
+    var $img = createModal.sinopsis();
+    var $container = createModal.container($img);
+    $img.attr('src', 'images/postales/bosque.jpg');
     modalInsert($container);
   });
 }
@@ -126,6 +139,7 @@ function modalReady() {
   modalTrailer();
   modalCarousel();
   modalCast();
+  modalSinopsis();
 }
 
 $(document).ready(modalReady);
